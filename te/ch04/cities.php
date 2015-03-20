863e22333b0ea34929d3d764b81c58c107613ced
Sonia98 <sonia.robert-nicoud@cpnv.ch>
2015-03-20 16:48:04 +0100

    Update and rename te1 to cities.php

--- te/ch04/cities.php
+++ te/ch04/cities.php
@@ -1,72 +1,95 @@
 <?php 
 /*----------------------------------------------------------------------------
-* Nom du programme: city
+* Nom du programme: cities.php
 *
 * Auteur : Sonia Robert-Nicoud
 * Date: 13.03.2015
 * Description: test où nous devons afficher notre table City.
-*
+* 
 *-----------------------------------------------------------------------------
 */
 ?>
+
 <?php
 define("DB_SERVER", 'localhost');
 define("DB_USER", 'cpnv');
 define("DB_PASSWORD", 'cpnv1234');
 define("DB_NAME", 'world');
 ?>
+<!DOCTYPE html>
 <html lang="fr">
+
 <head>
+
 <meta charset="UTF-8">
-<title>Test</title>
+
+<title>cities</title>
+
 </head>
+
 <body>
+    
 <?php
+
 //se connecter à la base de donnée et regarder qu'il n'y ait pas de problèmes pour s'y connecter.
-$dbh = new mysqli(DB_SERVER, DB_USER, DB_PWD, DB_NAME);
+
+$dbh = new mysqli(DB_SERVER, DB_USER, DB_PASSWORD, DB_NAME);
+
 if ($dbh->connect_errno) {
-die("Problèmes de connexion ({$dbh->connect_errno}) " .
-$dbh->connect_error);
+    die("Problèmes de connexion ({$dbh->connect_errno}) " .
+        $dbh->connect_error);
 }
-$dbh->close();
+
 
 $sql = "SELECT ID, Name, CountryCode, District FROM City WHERE CountryCode = 'CHE'
 ORDER BY Name";
-//petite phrase avant le tableau pour dire le nombres de villes qu'il y a.
-if ($result = $dbh->query($sql)) {
-$nbr = $result->num_rows;
-echo "Nous retrouvons" . $nbr . "ville(s) en Suisse";
-$result->close();
-}
-//Création de la table.
-?>
-<table>
-<tr>
-<td>ID</td>
-<td>Name</td>
-<td>CountryCode</td>
-<td>District</td>
-<td>Population from City</td>
-<td>edit.php</td>
-</tr>
-</table>
-<?php
-while($donnees = mysql_fetch_array($result))
-{
-//Affichage de la table
-?>
-<tr>
-<td><?php echo $donnees['ID'];?></td>
-<td><?php echo $donnees['Name'];?></td>
-<td><?php echo $donnees['CountryCode'];?></td>
-<td><?php echo $donnees['District'];?></td>
-<td><?php echo $donnees['Population from City'];?></td>
-<td><?php echo $donnees['edit.php'];?></td>
-</tr>
 
-<input type="submit" value="Enregistrer"></submit>
+//petite phrase avant le tableau pour dire le nombres de villes qu'il y a.
+
+if ($result = $dbh->query($sql)) {
+    $nbr = $result->num_rows;
+    echo "Nous retrouvons " . $nbr . " ville(s) en Suisse";
+}
+
+//Création de la table.
+
+?>
+
+<table>
+    <tr>
+        <td>ID</td>
+        <td>Name</td>
+        <td>CountryCode</td>
+        <td>District</td>
+        <td>Modifier</td>
+    </tr>
+
+<?php
+
+while ($city = $result->fetch_assoc()) 
+
+{
+
+//Affichage de la table
+
+?>
+
 <tr>
-<input type="submit" value="Annuler"></submit>
+    <td><?php echo $city['ID'];?></td>
+    <td><?php echo $city['Name'];?></td>
+    <td><?php echo $city['CountryCode'];?></td>
+    <td><?php echo $city['District'];?></td>
+    <td><a href="edit.php"> modifier </a></td>   
+ </tr>
+
+<?php
+
+}
+
+?>
+
+</table>
 
 </body>
+
 </html>
