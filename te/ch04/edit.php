b8de7bdec3a883a02eb6e37d13d125cb40d0ecfc
Sonia98 <sonia.robert-nicoud@cpnv.ch>
2015-03-27 16:11:37 +0100

    Update edit.php

--- te/ch04/edit.php
+++ te/ch04/edit.php
@@ -15,43 +15,86 @@ define("DB_USER", 'cpnv');
 define("DB_PASSWORD", 'cpnv1234');
 define("DB_NAME", 'world');
 ?>
-<!DOCTYPE html>
+<?php
+ //Déclaration des variables
+$id=$_GET['ID'];
+?>
 <html lang="fr">
 
 <head>
 
 <meta charset="UTF-8">
 
-<title>edit</title>
+<title>Chapitre 4 TE1</title>
 
 </head>
 
 <body>
-    
+
 <?php
 
-//se connecter à la base de donnée et regarder qu'il n'y ait pas de problèmes pour s'y connecter.
 
 $dbh = new mysqli(DB_SERVER, DB_USER, DB_PASSWORD, DB_NAME);
 
+
 if ($dbh->connect_errno) {
-    die("Problèmes de connexion ({$dbh->connect_errno}) " .
-        $dbh->connect_error);
+
+die("Problème de connexion ({$mysqli->connect_errno}) " .
+
+$mysqli->connect_error);
+
 }
 
+$sql = "SELECT ID, Name, CountryCode, District FROM City WHERE ID =$id
+ORDER BY Name";
+
+if ($result = $dbh->query($sql)) {
+
+//Gestion des erreurs lors de la transmission de la requête
+
+if (!$result) {
+
+$message = 'Requête invalide : ' . mysqli_error() . "\n";
+
+$message .= 'Requête complète : ' . $query;
+
+die($message);
+
+}
+
+//Création de la boucle permettant d'afficher tous les enregistrements
+
+$city = $result->fetch_assoc();
+
 ?>
- 
-    
-    ID:<input type="text" name=""><br>
-    Name:<input type="text" name="Name" value=""><br>
-    CountryCode:<input type="text" name="CountryCode" value=""><br>
-    District:<input type="text" name="District" value=""><br>
 
-    
-    <input type="submit" value="Enregistrer"></submit>
-    <tr>
-    <input type="submit" value="Annuler"></submit>   
-    
+<form method="post" action="cities.php">
+
+<p>ID:<input type="text" value=<?= $city['ID']; ?> /></p>
+
+<p>Name:<input type="text" value=<?= $city['Name']; ?> /></p>
+
+<p>CountryCode:<input type="text" value=<?= $city['CountryCode']; ?> /></p>
+
+<p>District:<input type="text" value=<?= $city['District']; ?> /></p>
+
+<input type="submit" value="Modifier" />
+
+<input type="submit" value="Enregistrer" />
+
+</form>
+
+<?php
+
+}
+
+$result->free();
+
+//Fermeture de la connexion à la base de données
+
+$dbh->close();
+
+?>
+
 </body>
-
-</html> 
+</html>
